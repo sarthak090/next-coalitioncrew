4e0db44d24efcd5b17dc7db39df20b037904f8d9
unknown <sarthakkaushik090@gmail.com>
2021-10-31 15:40:51 +0530

    styles fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  purge: ["./pages/**/*.tsx", "./pages/**/*.js", "./components/**/*.tsx"],
 
   darkMode: false, // or 'media' or 'class'
   theme: {
